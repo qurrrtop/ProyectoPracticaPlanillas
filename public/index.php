61130bc8e41bdca4060f0da47091cf5202db3316
maxip <mpercara3@gmail.com>
2025-08-19 18:58:35 -0300

    index bien

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
-<?php require_once __DIR__."./src/view/template/header.php" ?>
+<?php require_once __DIR__."/../src/view/template/header.php"; ?>
 
+<h1>texto de ejemplo</h1>
 
-<?php require_once __DIR__."./src/view/template/footer.php" ?>
+<?php require_once __DIR__."/../src/view/template/footer.php"; ?>

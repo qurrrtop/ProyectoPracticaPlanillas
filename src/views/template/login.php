e9a813f06aa26b584f60ee050499987233fe8566
Javier Maidana <Javiermaidana1234@gmail.com>
2025-08-20 18:37:14 -0300

    agregando cambios mios

---
+++ src/views/template/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/appwebplanilla/public/assets/css/login-style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Teachers:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>AppWebPlanilla</title>
+</head>
+<body>
+    <div class="form-card">
+        <div class="presentation">
+            <img class="logo-isfd" src="/appwebplanilla/public/assets/img/login/logo-isfd.png">
+
+            <p class="message">Inicie sesión para utilizar la Aplicación Web de Planillas de notas y asistencias!</p>
+        </div>
+
+        <div class="form">
+            <form action="" method="">
+                <p class="title">Bienvenido!</p>
+
+                <i class="fa-regular fa-id-card"></i>
+
+                <input class="inp-dni" type="text" placeholder="Ingrese su DNI">
+
+                <br>
+
+                <i class="fa-solid fa-lock"></i>
+                
+                
+                <input class="inp-pass" type="password" placeholder="Ingrese su contraseña">
+                
+                <br>
+
+                <button class="btn-submit" type="submit">Acceder</button>
+
+                <br>
+
+                <a class="link-pass" href="">Recuperar contraseña</a>
+
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>

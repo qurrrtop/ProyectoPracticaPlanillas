207d7d31a49ebc4dabb43fbe17ede5abdf07b367
Javier Maidana <Javiermaidana1234@gmail.com>
2025-08-24 14:56:18 -0300

    de contraseña a password

--- src/model/UsuarioModelo.php
+++ src/model/UsuarioModelo.php
@@ -1,41 +1,41 @@
 <?php
     
     class UsuarioModelo extends PersonaModelo {
-        protected $contraseña;
-        protected $usuario;
+        protected $password;
+        protected $user;
     }
 
-    public function __CONSTRUCT($id = null, $nombre = null, $apellido = null, $dni = null, $email = null, $telefono = null, $direccion = null, $fnacimiento = null, $contraseña = null, $usuario = null) {
+    public function __CONSTRUCT($id = null, $nombre = null, $apellido = null, $dni = null, $email = null, $telefono = null, $direccion = null, $fnacimiento = null, $password = null, $user = null) {
 
         parent::__CONSTRUCT($id, $nombre, $apellido, $dni, $email, $telefono, $direccion, $fnacimiento) {
-            $this -> contraseña = $contraseña;
-            $this -> usuario = $usuario;
+            $this -> password = $password;
+            $this -> user = $user;
         }
     }
 
-    public function getContraseña() {
-        return $this -> $contraseña;
+    public function getPassword() {
+        return $this -> $password;
     }
 
-    public function getUsuario() {
-        return $this -> $usuario;
+    public function getUser() {
+        return $this -> $user;
     }
 
     // MÉTODO SET USUARIO - CONTRASEÑA;
-    //validación para el usuario y contraseña (que no esté vacio, que sea string y que no sea nulo);
+    //validación para el user y password (que no esté vacio, que sea string y que no sea nulo);
     //la validación de longitud, caracteres, números, minúsculas y mayúsculas en otra capa.
 
-    public function setUsuario($usuario) {
-        if (empty($usuario) || !is_string($usuario) || $usuario === null) {
-            throw new Exception("El nombre de usuario es obligatorio y debe ser una cadena de texto");
+    public function setUser($user) {
+        if (empty($user) || !is_string($user) || $user === null) {
+            throw new Exception("El nombre de user es obligatorio y debe ser una cadena de texto");
         }
-        $this -> usuario = trim($usuario);
+        $this -> user = trim($user);
     }
 
-    public function setContraseña($contraseña) {
-        if (empty($contraseña) || !is_string($contraseña) || $contraseña === null) {
-            throw new Exception("La contraseña no es valida y debe ser una cadena de texto");
+    public function setPassword($password) {
+        if (empty($password) || !is_string($password) || $password === null) {
+            throw new Exception("La password no es valida y debe ser una cadena de texto");
         }
-        $this -> contraseña = trim($contraseña);
+        $this -> password = trim($password);
     }
 ?>

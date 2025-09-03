09a2c2afa88b0ce869785af0259385ec14a22837
maxip2 <mpercara3@gmail.com>
2025-09-03 19:16:56 -0300

    docente y coordinador model

--- src/model/DocenteModel.php
+++ src/model/DocenteModel.php
@@ -2,7 +2,20 @@
 
   class DocenteModel extends UsuarioModelo {
     
-  }
+    public function __construct( $idUsuario = null, $passwordHash = null, $userName = null,  $nombre = null, $apellido = null, $dni = null, $email = null, $telefono = null, $direccion = null, $fnacimiento = null ) {
+      
+      parent ::__construct( $idUsuario, $passwordHash, $userName, $nombre, $apellido, $dni, $email, $telefono, $direccion, $fnacimiento );
 
+    }
+
+    public function cargarNota() {
+
+    }
+
+    public function editarNota() {
+
+    }
+
+  }
 ?>
 <!-- asi se hace para heredar de un modelo a otro -->
